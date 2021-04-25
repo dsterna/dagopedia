02345111aca48d6d35a80e7fcc50272b92503fbf
dsterna <dsterna@kth.se>
2021-04-25 22:19:24 +0200

    deploying to github pages

---
+++ docs/precache-manifest.a2dd8a744d6f9dda5d2bc91e2b1087fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71d828e242ddc2519be8",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/css/main.729e6b70.chunk.css"
+  },
+  {
+    "revision": "71d828e242ddc2519be8",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/js/main.3bf426e8.chunk.js"
+  },
+  {
+    "revision": "8e2507e6606dcad2c366",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/js/runtime~main.3b40d21b.js"
+  },
+  {
+    "revision": "062f4814cc6848d11f1d",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/css/2.0604277d.chunk.css"
+  },
+  {
+    "revision": "062f4814cc6848d11f1d",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/js/2.ed687978.chunk.js"
+  },
+  {
+    "revision": "0df5066882793e7b1893e3adc393abbc",
+    "url": "https://dsterna.github.io/dagopedia/docs/static/media/WikipediaLogo.0df50668.svg"
+  },
+  {
+    "revision": "888d6b480762ddfbbc95dc129f0e0f1b",
+    "url": "https://dsterna.github.io/dagopedia/docs/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fd321cf03e364867f1e09ed584939d9.js"
+  "https://dsterna.github.io/dagopedia/docs/precache-manifest.a2dd8a744d6f9dda5d2bc91e2b1087fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://dsterna.github.io/dagopedia/docs/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
